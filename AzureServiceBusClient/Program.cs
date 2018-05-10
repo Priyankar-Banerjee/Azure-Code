89cad93bd98d9383483c8514578585222ecebc9a
Priyankar-Banerjee <38125471+Priyankar-Banerjee@users.noreply.github.com>
2018-05-10 23:34:29 +0530

    Update Program.cs

--- AzureServiceBusClient/Program.cs
+++ AzureServiceBusClient/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +11,7 @@ namespace AzureServiceBusClient
         static void Main(string[] args)
         {
             Console.WriteLine("Code coming soon !!");
+            Console.WriteLine("Code really coming soon !!");
             Console.ReadLine();
         }
     }
